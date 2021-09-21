89d439c9393391c34716aa8c3a4c9ab2ff25ab4a
tutdeputraw <anoboy.code@gmail.com>
2021-09-21 19:50:12 +0700

    make model, factory, migration, seeder, and controller

---
+++ database/factories/RestaurantTableFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\RestaurantTable;
+use Faker\Generator as Faker;
+
+$factory->define(RestaurantTable::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
