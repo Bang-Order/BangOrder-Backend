4e81efd3ebc0de4407ba3bb4624cf5940a1fae29
Akabane-kun <ursawarrior157@gmail.com>
2021-10-16 22:30:37 +0700

    fix: menu name faker

--- database/factories/MenuFactory.php
+++ database/factories/MenuFactory.php
@@ -10,11 +10,13 @@ $factory->define(Menu::class, function (Faker $faker) {
     $menu_category = MenuCategory::all()->find(rand(1, 10));
     $menu_category_id = $menu_category->id;
     $restaurant_id = $menu_category->restaurant_id;
+    $foodFaker = \Faker\Factory::create();
+    $foodFaker->addProvider(new \FakerRestaurant\Provider\id_ID\Restaurant($faker));
 
     return [
         'restaurant_id' => $restaurant_id,
         'menu_category_id' => $menu_category_id,
-        'name' => $faker->name(),
+        'name' => $foodFaker->foodName(),
         'description' => $faker->sentence(6),
         'price' => $faker->numberBetween($min = 10000, $max = 60000),
         'image' => 'https://picsum.photos/250?image=' . rand(25, 200),
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,8 +17,8 @@ class DatabaseSeeder extends Seeder
             MenuCategorySeeder::class,
             MenuSeeder::class,
             RestaurantTableSeeder::class,
-            //OrderSeeder::class,
-            //OrderItemSeeder::class
+            OrderSeeder::class,
+            // OrderItemSeeder::class
         ]);
     }
 }
