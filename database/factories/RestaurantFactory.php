70af543ceb95b564ddd86e2d4bfa843e3fc8a373
caesarjalu <caesarjalu123@gmail.com>
2021-10-17 19:24:55 +0700

    Make factory-made restaurant password to be hashed default password for factory-made restaurant: 123456

--- database/factories/RestaurantFactory.php
+++ database/factories/RestaurantFactory.php
@@ -4,12 +4,14 @@
 
 use App\Restaurant;
 use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Hash;
 
 $factory->define(Restaurant::class, function (Faker $faker) {
     return [
         'name' => $faker->company(),
         'email' => $faker->email(),
-        'password' => $faker->password(),
+        //'password' => $faker->password(),
+        'password' => Hash::make('123456'),
         'telephone_number' => $faker->phoneNumber(),
         'address' => $faker->address(),
         'image'=> $faker->imageUrl($width = 640, $height = 480),
