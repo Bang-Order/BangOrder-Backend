507c0e59deef9e6d59b815ec6a1eab671ead594b
caesarjalu <caesarjalu123@gmail.com>
2021-12-07 12:30:47 +0700

    Add forgot password API

---
+++ app/Http/Requests/Restaurant/ChangePasswordRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests\Restaurant;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Support\Facades\Gate;
+
+class ChangePasswordRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Gate::allows('restaurant-auth', $this->restaurant);
+    }
+
+    public function failedAuthorization()
+    {
+        throw new HttpResponseException(response()->json(['message' => 'This action is unauthorized.'], 401));
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'old_password' => ['required', 'string', 'min:8'],
+            'new_password' => ['required', 'string', 'min:8'],
+            'confirm_new_password' => ['required', 'string', 'min:8', 'same:new_password']
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -21,8 +21,10 @@ Route::prefix('auth')->group(function () {
     Route::post('/register/account', 'AuthController@registerAccount');
     Route::post('/register/profile', 'AuthController@registerProfile');
     Route::post('/logout', 'AuthController@logout');
-    Route::get('email/verify/{id}', 'AuthController@verifyEmail')->name('verification.verify');
-    Route::post('email/resend', 'AuthController@resendEmail')->name('verification.resend');
+    Route::get('/email/verify/{id}', 'AuthController@verifyEmail')->name('verification.verify');
+    Route::post('/email/resend', 'AuthController@resendEmail')->name('verification.resend');
+    Route::post('/password/send-email', 'AuthController@sendResetPassword');
+    Route::post('/password/reset', 'AuthController@resetPassword');
 });
 
 Route::prefix('notify')->group(function () {
@@ -30,14 +32,16 @@ Route::prefix('notify')->group(function () {
     Route::post('/withdraw', 'XenditController@withdrawNotify');
 });
 
-
 Route::post('/orders/history', 'OrderController@indexArray');
 
-Route::get('/restaurants/{restaurant}/dashboard', 'RestaurantController@showDashboard');
-Route::post('/restaurants/{restaurant}/withdraw', 'BankAccountController@withdraw');
-Route::get('/restaurants/{restaurant}/menu-categories/menus', 'MenuCategoryController@indexWithMenu');
-Route::get('/restaurants/{restaurant}/orders/history', 'OrderController@indexAll');
-Route::get('/restaurants/{restaurant}/tables/{table}/downloadQRCode', 'RestaurantTableController@getQRCode');
+Route::prefix('/restaurants/{restaurant}')->group(function () {
+    Route::get('/dashboard', 'RestaurantController@showDashboard');
+    Route::post('/withdraw', 'BankAccountController@withdraw');
+    Route::post('/change-password', 'RestaurantController@changePassword');
+    Route::get('/menu-categories/menus', 'MenuCategoryController@indexWithMenu');
+    Route::get('/orders/history', 'OrderController@indexAll');
+    Route::get('/tables/{table}/downloadQRCode', 'RestaurantTableController@getQRCode');
+});
 
 Route::apiResources([
     'restaurants' => 'RestaurantController',
